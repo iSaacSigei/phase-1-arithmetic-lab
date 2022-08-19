69fdc6bac9c4d26dc02f5dcfba4bb9f4c93f753a
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-19 11:30:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// multiply
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+
+//random integer greater than 0
+let random=Math.floor(Math.random()*10)
+
+//remainder equals 4
+let num3=24;
+let num4=5;
+let mod=num3%num4;
+
+//returns number 20 highest number
+let max =Math.max(1, 2, 3, 4, 5,10,15,20);
